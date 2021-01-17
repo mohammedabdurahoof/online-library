2415964e4eee45be409b5afb8fa228af28eeaaad
Mohammed Abdu Rahoof <mohammedabdurahoof527@gmail.com>
2021-01-17 06:57:55 -0500

    pwa finished

--- js/sworker.js
+++ js/sworker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
     e.waitUntill(
         caches.open("static").then(caches=>{
-            return caches.addAll(["./index.html","./css/style.css","./css/bootstrap.css","./css/bootstrap.min.css","./images/bennar.jpg","./images/download.jfif","./images/ICON.jpeg","./images/ICON64.png","./images/ICON512.png","./images/logo.png"]);
+            return caches.addAll(["./index.html","./About.html","./Books.html","./contact.html","./contribute.html","./E-books.html","./E-pepar.html","./megazin.html","./news.html","./privacy.html","./css/style.css","./css/bootstrap.css","./css/bootstrap.min.css","./images/bennar.jpg","./images/download.jfif","./images/ICON.jpeg","./images/ICON64.png","./images/ICON512.png","./images/logo.png"]);
 
         })
     )
